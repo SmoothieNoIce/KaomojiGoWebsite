ea446b0418113016061bf9bc8a0d45a26ddbc96a
SmoothieNoIce <aci5722@gmail.com>
2019-05-18 13:55:01 +0800

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -90,6 +90,11 @@ var pJS = function(tag_id, params){
     interactivity: {
       detect_on: 'canvas',
       events: {
+        onresize: {
+          enable: true,
+          density_auto: true,
+          density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+        },
         onhover: {
           enable: true,
           mode: 'grab'
