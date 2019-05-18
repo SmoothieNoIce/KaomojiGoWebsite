04b758346e033e025da8aad08a543f7578305c22
SmoothieNoIce <aci5722@gmail.com>
2019-05-18 14:17:59 +0800

    1
    1

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ particlesJS('particles-js',
       "value": "#6fddd8"
     },
     "shape": {
-      "type": ["text1", "text2", "text3", "text4", "text5", "text6", "text7", "text8", "text9", "text10", "text11", "text12", "text13", "text14", "text15", "text16", "text17", "text18", "text19", "text20"],
+      "type": "bubble",
       "stroke": {
         "width": 0,
         "color": "#000"
